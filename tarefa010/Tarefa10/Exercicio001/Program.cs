ad4ab60781ce2085bbad9041db936a55d663257f
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-07-06 20:28:44 -0300

    Implementa QuickSort e QuickSortRecursivo para Exercicio001

---
+++ tarefa010/Tarefa10/Exercicio001/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercicio001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            TestarQuickSortRecursivo(ObterArrayDesordenado());
+            stopwatch.Stop();
+
+            Console.WriteLine($"Quick sort recursivo: {stopwatch.Elapsed}");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            TestarQuickSort(ObterArrayDesordenado());
+            stopwatch.Stop();
+
+            Console.WriteLine($"Quick sort: {stopwatch.Elapsed}");
+        }
+
+        private static void TestarQuickSort(int[] arr)
+        {
+            new QuickSort().Sort(arr);
+        }
+
+        private static void TestarQuickSortRecursivo(int[] arr)
+        {
+            new QuickSortRecursivo().Sort(arr, 0, arr.Length - 1);
+        }
+
+        private static int[] ObterArrayDesordenado()
+        {
+            int[] arr = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
+            return arr;
+        }
+    }
+}
---
+++ tarefa010/Tarefa10/Exercicio001/QuickSortRecursivo.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercicio001
+{
+    public class QuickSortRecursivo
+    {
+        public int Partition(int[] arr, int left, int right)
+        {
+            int pivot;
+            pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left < right)
+                {
+                    int temp = arr[right];
+                    arr[right] = arr[left];
+                    arr[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        public void Sort(int[] arr, int left, int right)
+        {
+            int pivot;
+            if (left < right)
+            {
+                pivot = Partition(arr, left, right);
+                if (pivot > 1)
+                {
+                    Sort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Sort(arr, pivot + 1, right);
+                }
+            }
+        }
+    }
+}

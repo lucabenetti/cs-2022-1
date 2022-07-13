4e3ca9ef459b3b22bf5082a17e296e976ffa01ed
Luca Baccheschi Benetti <lucabbenetti@gmail.com>
2022-07-13 19:38:22 -0300

    Adiciona Exercicio001 de tarefa 12

---
+++ tarefa012/Tarefa12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarefa12
+{
+    internal class Program
+    {
+        //InputMismatchException do Java => FormatException
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dividendo = Convert.ToDecimal(Console.ReadLine());
+                var divisor = Convert.ToDecimal(Console.ReadLine());
+                var resultado = (dividendo / divisor);
+            }
+
+            catch (FormatException exception)
+            {
+                Console.WriteLine("Valor informado não númerico");
+            }
+            catch (ArithmeticException exception)
+            {
+                Console.WriteLine("Impossível dividir por zero");
+            }
+        }
+    }
+}
